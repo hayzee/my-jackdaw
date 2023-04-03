537eb0eabc4b0bc9c56961aa43320e3a91eec64f
hayzee <hayzee@users.noreply.github.com>
2023-04-03 11:26:52 +0100

    Producer

--- src/my_jackdaw/client/producer.clj
+++ src/my_jackdaw/client/producer.clj
@@ -1,29 +1,46 @@
 (ns my-jackdaw.client.producer
-  (:require [jackdaw.client :as jc]))
+  (:require [jackdaw.client :as jc])
+  (:import (org.apache.kafka.clients.producer KafkaProducer)))
 
 ; PRODUCER
 
-(def producers (atom {}))
+(defonce ^:private producers (atom {}))
 
 (def producer-config
   {"bootstrap.servers" "localhost:9092"
    "key.serializer" "org.apache.kafka.common.serialization.StringSerializer"
    "value.serializer" "org.apache.kafka.common.serialization.StringSerializer"
    "acks" "all"
-   "client.id" "some-client"})
+   "client.id" "bob2"})
 
 (defn create-producer
-  [name producer-config]
+  [producer-name producer-config]
   (if (get (deref producers) name)
     (ex-info "A producer of this name already exists" {:name name})
-    (swap! producers assoc name (jc/producer producer-config))))
+    (swap! producers assoc producer-name (jc/producer producer-config))))
+
+(defn list-producers
+  []
+  (keys @producers))
+
+(defn get-producer
+  [producer-name]
+  (get @producers producer-name))
 
 (defn produce!
   [producer-name topic key value]
-  (let [producer (get @producers producer-name)]
-    (jc/produce! producer topic key value)))
+  (if-let [producer (get @producers producer-name)]
+    (jc/produce! producer topic key value)
+    (throw (ex-info "No such producer" {:producer-name producer-name}))))
 
 (defn close-producer
   [producer-name]
   (let [producer (get @producers producer-name)]
-   (.close producer)))
+    (.close ^KafkaProducer producer)
+   (swap! producers dissoc producer-name)))
+
+(defn shutdown-producers
+  []
+  (doseq [producer-name (list-producers)]
+    (close-producer producer-name)))
+
